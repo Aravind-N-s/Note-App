a2ef04bbaa23ad6bc2c5cd1827013deb0c70d021
Aravind <aravind1808@gmail.com>
2019-08-08 20:07:19 +0530

    index changes

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const {mongoose} = require('./config/database') // mongoose without {} if single value is passed
+const app = express()
 //1st approach
 const router = require('./config/routes')
 //2nd approach
+const tagsRouter = require('./app/controllers/TagsController')
 const { usersRouter } = require('./app/controllers/userController') 
 const categoriesRouter = require('./app/controllers/categoriesController')
-const tagsRouter = require('./app/controllers/TagsController')
 
 //for heroku 
 const path = require("path")
 const port = process.env.port || 3001
-app.use(express.static(path.join(__dirname + "/client/build/index")))
-
-app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
-})
-
-
-const app = express()
-const port = 3005
 
 app.use(express.json())
 app.use(cors())
@@ -28,6 +20,12 @@ app.use('/', router) //1st approach
 app.use('/categories',categoriesRouter)// 2nd approach
 app.use('/tags',tagsRouter)
 app.use('/user',usersRouter)
+
+app.use(express.static(path.join(__dirname + "/client/build/index")))
+
+app.get("*", (req,res) =>{
+    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+})
 
 
 app.listen(port ,() =>{
