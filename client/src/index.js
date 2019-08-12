c966a443de2d65380c9985fc201b9cddb232f63c
Aravind <aravind1808@gmail.com>
2019-08-12 13:24:26 +0530

    error hunting

--- client/src/index.js
+++ client/src/index.js
@@ -8,14 +8,14 @@ import NotesRegister from './components/User/Register'
 import NotesAccount from './components/User/Account'
 import NotesLogout from './components/User/Logout'
 
-import CategoryList from './components/Category/List'
-import CategoryNew from './components/Category/New'
-import CategoryForm from './components/Category/Form'
+// import CategoryList from './components/Category/List'
+// import CategoryNew from './components/Category/New'
+// import CategoryForm from './components/Category/Form'
 
-import ShowNote from '../src/components/Notes/Show'
-import NoteNew from  './components/Notes/New'
-import NoteEdit from './components/Notes/Edit'
-import NotesList from './components/Notes/List'
+// import ShowNote from './components/Notes/Show'
+// import NoteNew from  './components/Notes/New'
+// import NoteEdit from './components/Notes/Edit'
+// import NotesList from './components/Notes/List'
 
 class App extends React.Component {
     constructor(props){
@@ -68,54 +68,54 @@ class App extends React.Component {
 
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <h1 className="navbar font-italic font-weight-bold shadow-lg p-3 mb-5 rounded" >
-                        <Link className ="text-dark" to="/notes" >My Notes App</Link>{this.handleShowAuth()}</h1>
-                        { this.state.isAuthenticated && (
-                            <div className ="container" >
-                                    <Popup trigger={<Link className=" btn btn-primary btn-lg  col-md-4"  to ="/notes/new"><h3> New Notes</h3></Link>} position = "right top" on="click">
-                                        <div>
-                                            <NoteNew />
-                                        </div>
-                                    </Popup>
-                                    <Link style={{marginLeft:10}} className=" btn btn-secondary btn-lg  col-md-4"  to ="/category"><h3>List Category</h3></Link>                           
-                                <Switch>
-                                <>
-                                    <Route path="/users/logout" render = {(props) => {
-                                        return <>
-                                            <NotesLogout {...props} handleAuth={this.handleAuth} />
-                                            <Redirect to="/"/>
-                                            </>
-                                    }}/>
-                                    <Route exact path="/notes" component={NotesList}/>
-                                    <Route exact strict path="/users/login" render={() => (<Redirect to="/notes"/>)} />
-                                    <Route exact strict path="/users/register" render={() => (<Redirect to="/notes"/>)} />
-                                    <Route exact strict path="/users/account" render={() => (<Redirect to="/notes"/>)} />
-                                    <Route exact strict path="/notes/new"/>
-                                    <Route path="/notes/edit/:id" exact component={NoteEdit} render={() => (<Redirect to="/notes/:id"/>)}  />
-                                    <Route path="/notes/:id" exact strict render = {(props) => {
-                                        return <ShowNote {...props} handleAuth={this.handleAuth} />
-                                    }}/>
-                                    <Route path="/category" component={CategoryList} exact={true}/>
-                                    <Route path="/category/new" component={CategoryNew} />  
-                                    <Route path="/category/edit/:id" render = {(props) => {
-                                        return <CategoryForm {...props} selectedCategory={'hello'}/>
-                                    }} exact={true}/>
-                                </>
-                                </Switch>
-                            </div>
-                        )}
-                        {!this.state.isAuthenticated &&(
-                            <Switch>
-                                <>
-                                    <Route exact path="/users/login" render={() => (<Redirect to="/" />)} />
-                                    <Route exact path="/users/register" render={() => (<Redirect to="/" />)} />
-                                    {/* <Route exact path="/" render={() => (<Redirect to="/error" />)} /> */}
-                                </>
-                            </Switch>
-                        )}                                             
-                </div>
+            // <BrowserRouter>
+            //     <div>
+            //         <h1 className="navbar font-italic font-weight-bold shadow-lg p-3 mb-5 rounded" >
+            //             <Link className ="text-dark" to="/notes" >My Notes App</Link>{this.handleShowAuth()}</h1>
+            //             { this.state.isAuthenticated && (
+            //                 <div className ="container" >
+            //                         <Popup trigger={<Link className=" btn btn-primary btn-lg  col-md-4"  to ="/notes/new"><h3> New Notes</h3></Link>} position = "right top" on="click">
+            //                             <div>
+            //                                 <NoteNew />
+            //                             </div>
+            //                         </Popup>
+            //                         <Link style={{marginLeft:10}} className=" btn btn-secondary btn-lg  col-md-4"  to ="/category"><h3>List Category</h3></Link>                           
+            //                     <Switch>
+            //                     <>
+            //                         <Route path="/users/logout" render = {(props) => {
+            //                             return <>
+            //                                 <NotesLogout {...props} handleAuth={this.handleAuth} />
+            //                                 <Redirect to="/"/>
+            //                                 </>
+            //                         }}/>
+            //                         <Route exact path="/notes" component={NotesList}/>
+            //                         <Route exact strict path="/users/login" render={() => (<Redirect to="/notes"/>)} />
+            //                         <Route exact strict path="/users/register" render={() => (<Redirect to="/notes"/>)} />
+            //                         <Route exact strict path="/users/account" render={() => (<Redirect to="/notes"/>)} />
+            //                         <Route exact strict path="/notes/new"/>
+            //                         <Route path="/notes/edit/:id" exact component={NoteEdit} render={() => (<Redirect to="/notes/:id"/>)}  />
+            //                         {/* <Route path="/notes/:id" exact strict render = {(props) => {
+            //                             return <ShowNote {...props} handleAuth={this.handleAuth} />
+            //                         }}/> */}
+            //                         <Route path="/category" component={CategoryList} exact={true}/>
+            //                         <Route path="/category/new" component={CategoryNew} />  
+            //                         <Route path="/category/edit/:id" render = {(props) => {
+            //                             return <CategoryForm {...props} selectedCategory={'hello'}/>
+            //                         }} exact={true}/>
+            //                     </>
+            //                     </Switch>
+            //                 </div>
+            //             )}
+            //             {!this.state.isAuthenticated &&(
+            //                 <Switch>
+            //                     <>
+            //                         <Route exact path="/users/login" render={() => (<Redirect to="/" />)} />
+            //                         <Route exact path="/users/register" render={() => (<Redirect to="/" />)} />
+            //                         {/* <Route exact path="/" render={() => (<Redirect to="/error" />)} /> */}
+            //                     </>
+            //                 </Switch>
+            //             )}                                             
+                // </div>
             </BrowserRouter>
         )
     }
