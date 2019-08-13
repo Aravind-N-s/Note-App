5d13106749b93bd0fc977a5251559ea10cdc053a
Aravind <aravind1808@gmail.com>
2019-08-13 20:30:41 +0530

    List

--- client/src/components/notes/List.js
+++ client/src/components/notes/List.js
@@ -1,35 +1,36 @@
 import React from 'react'
-import axios from '../../config/axios'
-import {Link} from 'react-router-dom'
+// import axios from '../../config/axios'
+// import {Link} from 'react-router-dom'
 
 class NotesList extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            notes: []
-        }
-    }
-    componentDidMount(){
-        axios.get('/notes',{
-            headers:{
-                'x-auth':localStorage.getItem('userAuthToken')
-            }
-        })
-        .then(response => {
-            this.setState(() => ({
-                notes: response.data
-            }))
-        })
-    }
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         notes: []
+    //     }
+    // }
+    // componentDidMount(){
+    //     axios.get('/notes',{
+    //         headers:{
+    //             'x-auth':localStorage.getItem('userAuthToken')
+    //         }
+    //     })
+    //     .then(response => {
+    //         this.setState(() => ({
+    //             notes: response.data
+    //         }))
+    //     })
+    // }
 
 
     render(){
         return(
-            <ul style ={{marginTop: "10px", width: "18rem"}} className = "list-group list-group-flush border border-dark">
-                {this.state.notes.map(note => {
-                    return <li className="list-group-item border border-dark" key={note._id}><Link to={`/notes/show/${note._id}`}>{<h3>{note.title}</h3>}</Link></li>
-                })}
-            </ul> 
+            // <ul style ={{marginTop: "10px", width: "18rem"}} className = "list-group list-group-flush border border-dark">
+            //     {this.state.notes.map(note => {
+            //         return <li className="list-group-item border border-dark" key={note._id}><Link to={`/notes/show/${note._id}`}>{<h3>{note.title}</h3>}</Link></li>
+            //     })}
+            // </ul> 
+            Hello
         )
     }
 }
