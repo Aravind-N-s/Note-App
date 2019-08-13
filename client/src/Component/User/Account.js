e40f5f34164988d2d1d5a6605f9642cf3774e085
Aravind <aravind1808@gmail.com>
2019-08-13 23:54:28 +0530

    hosting

--- client/src/Component/User/Account.js
+++ client/src/Component/User/Account.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import axios from '../../Config/axios';
+import axios from '../../Config/axios'
 
 class NotesAccount extends React.Component{
     constructor(){
