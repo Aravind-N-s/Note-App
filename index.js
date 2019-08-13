dbdabd1e8629a89a554898ab2c4186f58cb61b69
Aravind <aravind1808@gmail.com>
2019-08-13 23:26:40 +0530

    File Name

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use('/categories',categoriesRouter)// 2nd approach
 app.use('/tags',tagsRouter)
 app.use('/user',usersRouter)
 
-app.use(express.static(path.join(__dirname,"client/build")))
+app.use(express.static(path.join(_dirname,"client/build")))
 
 app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    res.sendFile(path.join(_dirname + "/client/build/index.html"))
 })
 
 
